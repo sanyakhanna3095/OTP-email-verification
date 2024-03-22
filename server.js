981b3f7089c90ee5a2c20b38bbcdaab515d95ec9
Sanya Khanna <102346888+sanyakhanna3095@users.noreply.github.com>
2024-03-22 17:06:35 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const app = express();
+require("dotenv").config();
+
+app.set("view engine", "hbs");
+app.use(express.json());
+app.use(express.urlencoded());
+
+let otp;
+function generateOtp() {
+  otpVal = Math.floor(1000 + Math.random() * 9000);
+  console.log(otpVal);
+  return otpVal;
+}
+
+app.get("/", (req, res) => {
+  res.render("otp");
+});
+
+app.post("/send", async (req, res) => {
+
+
+  otp = generateOtp();
+  const recemail = req.body.email;
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "sanyakhanna2022@gmail.com",
+      pass: process.env.PASS,
+    },
+  });
+  const mail = {
+    to: recemail,
+    from: "sanyakhanna2022@gmail.com",
+    subject: "Hi nodemailer",
+    text: `${otp}`,
+  };
+
+  try {
+    await transporter.sendMail(mail);
+    res.send("otp sent successfully :D");
+  } catch (err) {
+    res.send("OOPS! otp not sent");
+  }
+});
+
+app.post("/verify", (req, res) => {
+  if (req.body.otp == otp) {
+    res.send("You entered correct otp! :)");
+  } else {
+    res.send("otp is incorrect");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("http://localhost:3000");
+});
